b454fa6a8610f0c5a74b08227def2c2c6a9b28aa
Gabriel <gverbel6@gmail.com>
2020-08-26 21:49:12 -0500

    Mejoras: - pasar el commando a un static method - creacion del metodo delete - agregar impresiones de metodos (futuro a log)

--- CloudManager/cloudManager.py
+++ CloudManager/cloudManager.py
@@ -7,7 +7,7 @@ from google.cloud import storage
 
 class StorageManager():
 
-    def __init__(self, bucket_name,credential_path='proy_cred.json'):
+    def __init__(self, bucket_name):
 
         warning =   """
                     This class is in charge of managing the GCP storage. 
@@ -18,23 +18,27 @@ class StorageManager():
                     
                     """
 
-        credential_path = os.path.abspath(credential_path)
-
-        self.__windows_command = f'$env:GOOGLE_APPLICATION_CREDENTIALS="{credential_path}"'
-        self.__linux_command =f'export GOOGLE_APPLICATION_CREDENTIALS="{credential_path}"'
         self.bucket_name = bucket_name
 
-        if platform.system() == 'Windows':
-            self.credential_command = self.__windows_command
-        else:
-            self.credential_command = self.__linux_command
-
         print(warning)
+
+    @staticmethod
+    def credential_command(credential_path='proy_cred.json'):
+        '''
+        Prints the command to execute to validate credential depending on OS.
+        '''
+        credential_path = os.path.abspath(credential_path)
+
+        if platform.system() == 'Windows':
+            return f'Credential command: $env:GOOGLE_APPLICATION_CREDENTIALS="{credential_path}"'
+        else:
+            return f'Credential command: export GOOGLE_APPLICATION_CREDENTIALS="{credential_path}"'
+            
+
 
     def list_storage_objects(self):
         """
         Lists all the blobs (objects) in the bucket.
-            bucket_name: Name of the storage bucket. As: pdfs
         """
         storage_client = storage.Client()
         # Note: Client.list_blobs requires at least package version 1.17.0.
@@ -44,8 +48,8 @@ class StorageManager():
     
     def upload_object(self, source_file_path, destination_blob_name):
         ''' Uploads a file to the bucket.
-                source_file_path = "local/path/to/file"
-                destination_blob_name = "storage-object-name"
+                source_file_path = path to the file to be uploaded on the storage bucket
+                destination_blob_name = name used to store file in the storage bucket
         '''
         storage_client = storage.Client()
         bucket = storage_client.bucket(self.bucket_name)
@@ -53,19 +57,34 @@ class StorageManager():
 
         blob.upload_from_filename(source_file_path)
 
+        print(f'File: {source_file_path} subido como {destination_blob_name}')
+
 
     def download_object(self, source_blob_name, destination_file_path):
-            ''' Downloads a blob from the bucket.
-                    source_blob_path = "storage-object-name"
-                    destination_file_name = "local/path/to/file"
-            '''
-            storage_client = storage.Client()
+        ''' Downloads a blob from the bucket.
+                source_blob_path = name of the stored object 
+                destination_file_name = path where file will be downloaded
+        '''
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(self.bucket_name)
+        blob = bucket.blob(source_blob_name)
 
-            bucket = storage_client.bucket(self.bucket_name)
-            blob = bucket.blob(source_blob_name)
+        blob.download_to_filename(destination_file_path)
 
-            blob.download_to_filename(destination_file_path)
+        print(f'Blob: {source_blob_name} descargado en {destination_file_path}')
 
+
+    def delete_object(self, source_blob_name):
+        ''' Deletes a blob in GCP:
+                source_blob_name: file/blob name in storage bucket
+        '''
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(self.bucket_name)
+        blob = bucket.blob(source_blob_name)
+        blob.delete()
+
+        print(f'Blob: {source_blob_name} eliminado')
+        
 
 
 if __name__ == '__main__':
