9d8c5c33292bcbb053cc31af39eb0ec0ebf44431
Gabriel <gverbel6@gmail.com>
2020-09-02 22:19:16 -0500

    Update of cloud manager to enable object metadata editing

--- CloudManager/cloudManager.py
+++ CloudManager/cloudManager.py
@@ -34,7 +34,22 @@ class StorageManager():
         else:
             return f'Credential command: export GOOGLE_APPLICATION_CREDENTIALS="{credential_path}"'
             
+    def get_blobs(self):
+        """
+        Returns all the blobs with their metadata
+        """
+        storage_client = storage.Client()
+        blobs = storage_client.list_blobs(self.bucket_name)
+        return blobs
 
+    def get_blob(self, blob_name):
+        """
+        Returns a specific blob
+        """
+        storage_client = storage.Client()
+        blobs = storage_client.list_blobs(self.bucket_name)
+        blob = [blob for blob in blobs if blob.name == blob_name][0]
+        return blob
 
     def list_storage_objects(self):
         """
@@ -46,7 +61,7 @@ class StorageManager():
 
         return [blob.name for blob in blobs]
     
-    def upload_object(self, source_file_path, destination_blob_name):
+    def upload_object(self, source_file_path, destination_blob_name, extracted=False):
         ''' Uploads a file to the bucket.
                 source_file_path = path to the file to be uploaded on the storage bucket
                 destination_blob_name = name used to store file in the storage bucket
@@ -55,10 +70,20 @@ class StorageManager():
         bucket = storage_client.bucket(self.bucket_name)
         blob = bucket.blob(destination_blob_name)
 
+        blob.metadata = {'extracted': extracted}
+
         blob.upload_from_filename(source_file_path)
 
         print(f'File: {source_file_path} subido como {destination_blob_name}')
 
+    def update_blob_extracted(self, blob_name, file_path):
+        """
+        Updates the blobs metadata to indicate it has been extracted already
+        params: 
+            file_path: Path of the file to be updated
+            blob_name: name of the blob on storage to be updated
+        """
+        upload_object(file_path, blob_name, extracted=True )
 
     def download_object(self, source_blob_name, destination_file_path):
         ''' Downloads a blob from the bucket.
@@ -84,6 +109,10 @@ class StorageManager():
         blob.delete()
 
         print(f'Blob: {source_blob_name} eliminado')
+
+
+
+
         
 
 
