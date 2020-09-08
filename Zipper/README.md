482eb67b6dcc5e1c4fc0d6c1df314e3c5af055b7
Gabriel <gverbel6@gmail.com>
2020-09-07 22:20:45 -0500

    Creacion de archivo bash para zippear todos los archivos de un bucket

---
+++ Zipper/README.md
@@ -0,0 +1,7 @@
+# Zipper
+This module is used to download all files in the storage to zip them and then back to storage to use later by Google Colab. 
+
+## How to run
+
+This module is meant to be run on the gcloud console for easier management. The content on this module is a bash file. 
+The commands can be performed individuali on the console or downloaded and run. 
---
+++ Zipper/zipper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Params:
+NEW_FOLDER=$1 # Folder to download {Like: new_folder}
+BUCKET_NAME=$2 # Bucket to download or folder in bucket {gs://bucket}
+ZIP_NAME=$3 # Name of the .zip file to create {download.zip}
+
+
+mkdir $NEW_FOLDER
+cd $NEW_FOLDER
+gsutil cp -r $BUCKET_NAME .
+zip -r $ZIP_NAME .
+gsutil cp -r $ZIP_NAME $BUCKET_NAME
+gsutil -m acl set -R -a public-read $BUCKET_NAME/$ZIP_NAME
+echo "Public URL: "
+echo "storage.googleapis.com"/${BUCKET_NAME:5}/$ZIP_NAME
